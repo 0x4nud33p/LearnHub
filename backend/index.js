fcc4520e23a17cddfd0c70e68638cde697edc6e1
[anudeep avula] <anudeepavula009@gmail.com>
2025-06-26 16:37:49 +0530

    feat:routers added

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,37 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const cors = require("cors");
-const bodyParser = require("body-parser");
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const DBConnection = require('./config/connect')
+const path = require("path");
+const fs = require('fs')
 
-// Load env vars
-dotenv.config();
+const app = express()
+dotenv.config()
 
-const app = express();
+//////connection of DB/////////
+DBConnection()
 
-// Middleware
-app.use(cors());
-app.use(bodyParser.json());
+const PORT = process.env.PORT 
 
 
-const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI;
+//////middleware/////////
+app.use(express.json())
+app.use(cors())
 
-mongoose
-  .connect(MONGO_URI)
-  .then(() => {
-    console.log("MongoDB connected ✅");
-    app.listen(PORT, () => console.log(`Server running on port ${PORT} 🚀`));
-  })
-  .catch((err) => console.error("MongoDB connection error ❌:", err));
+const uploadsDir = path.join(__dirname, "uploads");
+
+// Create uploads folder if it doesn’t exist
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir);
+}
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+
+///ROUTES///
+app.use('/api/admin', require('./routers/adminRoutes'))
+app.use('/api/user', require('./routers/userRoutes'))
+
+
+
+app.listen(PORT, () => console.log(`running on ${PORT}`))
